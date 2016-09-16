5c585f43f1fe01b174876a14cdcbdbc20e6c4ae4
Hedi Nasr <h.nsr69@gmail.com>
2016-09-16 09:28:08 +0200

    update package.json & add command line args

--- main.js
+++ main.js
@@ -1,16 +1,32 @@
 var Alldebrid = require('./lib/alldebrid.js');
+var fs = require('fs');
 
-var debridme = new Alldebrid();
+var alldebrid = new Alldebrid();
 
 /**
+ * How to use : alldebrid <url>
+ * You have to put your login informations in user.json file
  *
- *
- *
+ * TODO use env var instead of user.json
+ * TODO add some options
  *
  */
 
-debridme.connect('', '', function(err) {
-    if (!err) {
-        debridme.debrid();
-    }
-});
+var argv = require('minimist')(process.argv.slice(2));
+
+if (argv._.length == 0) {
+    console.log('Usage: alldebrid <url>');
+} else {
+    // Check if user is connected
+    fs.readFile('./user.json', 'utf-8', function(err, data) {
+        if (err)
+            return console.log('There is no user.json : ' + err);
+        var user = JSON.parse(data);
+        alldebrid.connect(user.login, user.password, function(err) {
+            if (err)
+                return console.log(err);
+            var url = argv._[0];
+            alldebrid.debrid(url);
+        });
+    });
+}
